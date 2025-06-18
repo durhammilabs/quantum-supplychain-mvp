911fd1334f399f523314f09f686d5b8b56408dd9
durhammilabs <michael@quantumprooftech.com>
2025-06-18 09:47:00 -0400

    Update verify_token.py

--- verify_token.py
+++ verify_token.py
@@ -16,3 +16,8 @@ def verify_token(token: dict, public_key):
         return True
     except Exception:
         return False
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Temporary in-memory keys (for demo purposes)
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key = private_key.public_key()
